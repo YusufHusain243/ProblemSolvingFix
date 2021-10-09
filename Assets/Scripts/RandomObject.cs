e245824da6ceb5ff4e5f89a75b1319e0edfc71c6
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-09 09:51:35 +0700

    Problem 6

---
+++ Assets/Scripts/RandomObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObject : MonoBehaviour
+{
+    public GameObject gameObject;
+    public float minX, maxX, minY, maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int gameObjectAmount = Random.Range(0, 10);
+        for (int i = 0; i < gameObjectAmount; i++)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            Instantiate(gameObject, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+
+}
