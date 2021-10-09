cdcfb388be1d4d4a423e063170ceaa3cae6ac54f
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-09 09:45:20 +0700

    Problem_5

---
+++ Assets/Scripts/Problem_5.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem_5 : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float minX, maxX, minY, maxY;
+    Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vertic = Input.GetAxis("Vertical");
+
+        if (horiz != 0 || vertic != 0)
+        {
+            Vector2 direction = new Vector2(horiz, vertic);
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+        else if (horiz == 0 || vertic == 0)
+        {
+            InputMouse();
+        }
+    }
+
+    void InputMouse()
+    {
+        pos = Input.mousePosition;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+
+        if (pos.x > minX && pos.x < maxX && pos.y > minY && pos.y < maxY)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
