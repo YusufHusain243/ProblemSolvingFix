13f7d86dfe5bacf7607935803b1136b89d5935c4
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-09 09:38:32 +0700

    Problem_4

---
+++ Assets/Scripts/Problem_4.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem_4 : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vertic = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horiz, vertic);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
