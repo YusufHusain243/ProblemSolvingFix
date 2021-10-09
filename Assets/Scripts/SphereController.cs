497e87ed7c15f4cc5196c7c38b2d57244c17c005
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-09 09:07:47 +0700

    Problem_2

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0, 360), transform.eulerAngles.z);
+        Vector3 force = transform.forward;
+        force = new Vector3(force.x, 1, force.z);
+
+        rb.AddForce(force * speed, ForceMode2D.Impulse);
+    }
+
+}
